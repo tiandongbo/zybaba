386a23ff058dd5675e4ed20cc9c3a3b488165714
zybaba <tiandongbo@aliyun.com>
2020-09-10 23:57:53 +0800

    114. 二叉树展开为链表 整体观，树的递归

---
+++ src/main/java/com/zybaba/leetcode/tree/FlattenTest.java
@@ -0,0 +1,34 @@
+package com.zybaba.leetcode.tree;
+
+/**
+ * @author tiandongbo
+ * @date 2020/09/10 22:29
+ * 114. 二叉树展开为链表
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+ */
+public class FlattenTest {
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return ;
+        }
+        //将根节点的左子树变成链表
+        flatten(root.left);
+        //将根节点的右子树变成链表
+        flatten(root.right);
+        TreeNode temp = root.right;
+        //把树的右边换成左边的链表
+        root.right = root.left;
+        //记得要将左边置空
+        root.left = null;
+        //找到树的最右边的节点
+        while(root.right != null) root = root.right;
+        //把右边的链表接到刚才树的最右边的节点
+        root.right = temp;
+
+        // 作者：Geralt_U
+        // 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/114-er-cha-shu-zhan-kai-wei-lian-biao-by-ming-zhi-/
+        // 来源：力扣（LeetCode）
+        // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+
+}
