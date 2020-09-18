77a6167f7cba9c4530f993728ba9fe3ba5c937ef
zybaba <tiandongbo@aliyun.com>
2020-09-18 23:11:09 +0800

    括号生成 深入理解递归和回溯

---
+++ src/main/java/com/zybaba/leetcode/backtrack/GenerateParenthesisTest.java
@@ -0,0 +1,75 @@
+package com.zybaba.leetcode.backtrack;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tiandongbo
+ * @date 2020/09/18 22:20
+ * 22. 括号生成
+ */
+public class GenerateParenthesisTest {
+    public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList<String>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+
+    @Test
+    public void testCase01() {
+        int n = 3;
+        GenerateParenthesisTest solution = new GenerateParenthesisTest();
+        List<String> list;
+        list = solution.generateParenthesis(n);
+        System.out.println(list);
+
+    }
+}
